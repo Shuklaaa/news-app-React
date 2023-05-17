20899e8061b9820fa290cdb768a6ccc29a94967e
Shuklaaa <shuklaabhishek0404@gmail.com>
2023-05-17 10:46:57 +0530

    Updating source as a badge

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,23 +2,45 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/05/14/cropped/16-9/asteroid_1681212528521_1684061754139.jpg?impolicy=new-ht-20210112&width=1600/":imageUrl} className="card-img-top" alt="..." />
-          <div class="card-header bg-black text-white">SOURCE : {source}</div>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.hindustantimes.com/tech/img/2023/05/14/cropped/16-9/asteroid_1681212528521_1684061754139.jpg?impolicy=new-ht-20210112&width=1600/"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          {/* <div class="card-header bg-black text-white">SOURCE : {source}</div> */}
           <div className="card-body">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{left: '90%', zIndex: '1'}}>
+              {source}
+            </span>
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p className="card-text"><small className="text-muted">By {author? author : "UNKNOWN"} on {new Date(date).toUTCString()}</small></p>
-            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "UNKNOWN"} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
               READ MORE
             </a>
           </div>
         </div>
 
-{/* <div className="card text-bg-dark">
+        {/* <div className="card text-bg-dark">
   <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/05/14/cropped/16-9/asteroid_1681212528521_1684061754139.jpg?impolicy=new-ht-20210112&width=1600/":imageUrl} className="card-img" alt="..."/>
   <div className="card-img-overlay">
     <h5 className="card-title">{title}</h5>
